0b6531aea86ff843cb58d64c16e6cb54ba897c48
AntonioDeveloper <37427385+AntonioDeveloper@users.noreply.github.com>
2020-08-19 00:15:57 -0300

    Add files via upload

---
+++ src/components/Calendario/Calendario.js
@@ -0,0 +1,59 @@
+import './input-moment.min.css';
+import React, { Component } from 'react';
+import moment from 'moment';
+import { BigInputMoment } from 'react-input-moment';
+import './pt-br'
+
+export default class Calendario extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      inputMoment: moment(),
+      bigInputMoment: moment(),
+      datePickerMoment: moment(),
+      datePickerRangeStartMoment: moment().subtract(3, 'days'),
+      datePickerRangeEndMoment: moment(),
+      timePickerMoment: moment(),
+      showSeconds: false,
+      locale: moment.locale('fr'),
+      size: 'medium'
+    };
+  }
+
+  render() {
+    let { bigInputMoment, showSeconds, locale, size } = this.state;
+    let wrapperClass = 'wrapper ' + size;
+
+    return (
+      <div className="app">
+        <div className="header">Agendamento de entrega</div>
+        <input
+          className="output"
+          type="text"
+          value={bigInputMoment.format('llll')}
+          readOnly
+        />
+        <div className={wrapperClass}>
+          <BigInputMoment
+            moment={bigInputMoment}
+            locale={locale}
+            showSeconds={showSeconds}
+            onChange={mom => this.setState({ bigInputMoment: mom })}
+          />
+        </div>
+        <br />
+      </div>
+    );
+  }
+
+  handleShowSeconds(e) {
+    this.setState({ showSeconds: e.target.checked });
+  }
+
+  handleLocale(e) {
+    this.setState({ locale: e.target.value });
+  }
+}
+
+// ReactDOM.render(<Calendario />, document.getElementById('app'));
